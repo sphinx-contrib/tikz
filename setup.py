5ac8c7134c865347b6ca26d7ea0eac26b3e423a4
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2013-01-12 11:56:40 +0200

    Clean setup.py and conf.py

--- setup.py
+++ setup.py
@@ -3,44 +3,54 @@
 from setuptools import setup, find_packages
 import sys
 
+LONG_DESCRIPTION = \
+'''
+This package contains the tikz Sphinx extension, which enables the use
+of the PGF/TikZ LaTeX package to draw nice pictures.
+'''
+
+NAME         = 'sphinxcontrib-tikz'
+DESCRIPTION  = 'TikZ extension for Sphinx'
+VERSION      = '0.4.1'
+AUTHOR       = 'Christoph Reller'
+AUTHOR_EMAIL = 'creller@ee.ethz.ch'
+URL          = 'https://bitbucket.org/philexander/tikz'
+DOWNLOAD     = 'http://pypi.python.org/pypi/sphinxcontrib-tikz'
+LICENSE      = 'BSD'
+REQUIRES     = ['Sphinx']
+CLASSIFIERS  = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Documentation',
+    'Topic :: Utilities',
+    ]
+
 # Use 2to3 for Python 3 without warnings in Python 2
 extra = {}
 if sys.version_info >= (3,):
     extra['use_2to3'] = True
     
-long_desc = '''
-This package contains the tikz Sphinx extension, which enables the use
-of the PGF/TikZ LaTeX package to draw nice pictures.
-'''
-
-requires = ['Sphinx>=0.6']
-
 setup(
-    name='sphinxcontrib-tikz',
-    version='0.4',
-    url='http://people.ee.ethz.ch/~creller/web/tricks/sphinx-tikz.html',
-    download_url='http://pypi.python.org/pypi/sphinxcontrib-tikz',
-    license='BSD',
-    author='Christoph Reller',
-    author_email='creller@ee.ethz.ch',
-    description='TikZ extension for Sphinx',
-    long_description=long_desc,
+    name=NAME,
+    version=VERSION,
+    url=URL,
+    download_url=DOWNLOAD,
+    license=LICENSE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
     zip_safe=False,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Documentation',
-        'Topic :: Utilities',
-    ],
+    classifiers=CLASSIFIERS,
     platforms='any',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=requires,
+    install_requires=REQUIRES,
     namespace_packages=['sphinxcontrib'],
     **extra
 )
