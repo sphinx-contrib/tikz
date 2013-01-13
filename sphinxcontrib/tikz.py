41116991ba7968674e749bf285607334c018dbb4
Christoph Reller <christoph.reller@gmail.com>
2013-01-13 15:17:16 +0100

    changed author email

--- sphinxcontrib/tikz.py
+++ sphinxcontrib/tikz.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2011 by Christoph Reller. All rights reserved.
+# Copyright (c) 2012-2013 by Christoph Reller. All rights reserved.
 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -28,23 +28,15 @@
 # policies, either expressed or implied, of Christoph Reller.
 
 """
-    tikz
-    ~~~~
+    sphinxcontrib.tikz
+    ~~~~~~~~~~~~~~~~~~
 
-    Render TikZ pictures
+    Draw pictures with the `TikZ/PGF LaTeX package.
 
-    We make use of
+    See README.rst file for details
 
-    * latex
-    * pdftoppm (part of Poppler)
-    * pnmcrop (part of Netpbm)
-    * pnmtopng (part of Netpbm)
-
-    Author: Christoph Reller
-    Email: creller@ee.ethz.ch
-    Version: 0.4
-    $Date: 2010-12-17 15:32:57 +0100 (Fri, 17 Dec 2010) $
-    $Revision$
+    Author: Christoph Reller <christoph.reller@gmail.com>
+    Version: 0.4.1
 """    
 
 import tempfile
