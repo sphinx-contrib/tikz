dfb32ec86031d1b3d544516b0faddc54b57b03fd
Sean Farley <sean.michael.farley@gmail.com>
2014-06-10 12:57:16 -0500

    tikz: fallback to image magick if netpbm not found

--- sphinxcontrib/tikz.py
+++ sphinxcontrib/tikz.py
@@ -382,5 +382,10 @@ def setup(app):
     app.add_config_value('tikz_latex_preamble', '', 'html')
     app.add_config_value('tikz_tikzlibraries', '', 'html')
     app.add_config_value('tikz_transparent', True, 'html')
-    app.add_config_value('tikz_proc_suite', 'Netpbm', 'html')
+
+    # fallback to another value depending what is on the system
+    suite = 'Netpbm'
+    if not which('pnmcrop'):
+        suite = 'ImageMagick'
+    app.add_config_value('tikz_proc_suite', suite, 'html')
     app.connect('build-finished', cleanup_tempdir)
