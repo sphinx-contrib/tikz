4ea88d4d27d7cb3550a5acfe7aeb7c3be0a9ca6b
Sean Farley <sean.michael.farley@gmail.com>
2014-06-10 12:56:48 -0500

    tikz: add which method for getting binaries from the path
    This is currently unused but upcoming patches will utilize this to find a
    default setting for 'Netpbm' or 'ImageMagick' depending if the command is
    found.

--- sphinxcontrib/tikz.py
+++ sphinxcontrib/tikz.py
@@ -352,6 +352,24 @@ def cleanup_tempdir(app, exc):
     except Exception:
         pass
 
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
 def setup(app):
     app.add_node(tikz,
                  html=(html_visit_tikz, depart_tikz),
